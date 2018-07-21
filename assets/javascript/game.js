e7449cf20e99fe6c086bc59b8de90c59f504ab13
Kristen Truyol <kristentruyol@Kristens-Air.centurylink.net>
2018-07-21 18:09:23 -0400

    loaded images and adjusted colors and placements

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+//array defining heros
+    //defining ATTACK POWER to all heroes (all different)
+    //defining HEALTH POINTS to all heroes (all differet)
+    //defining COUNTER ATTACK POWER to all heroes (all different)
+var heroes = {
+    pacmanPlayer: {
+        name: "pacman",
+        health: 125,
+        attack: 9,
+        counterAttack: 20
+    },
+    donkeykongPlayer: {
+        name: "donkey kong",
+        health: 155,
+        attack: 7,
+        counterAttack: 22
+    },
+    oregontrailPlayer: {
+        name: "oregon trail",
+        health: 130,
+        attack: 12,
+        counterAttack: 3
+    },
+    qbertPlayer: {
+        name: "q-bert",
+        health: 105,
+        attack: 10,
+        counterAttack: 16
+    }
+
+}
+
+//move first hero to user's character section
+
+//move opponent to defender area
+
+//attack button value
+
+//immediate counterattack value
+
+//if user HEALTH POINTS 0 or less, game over
+
+//if defender HEALTH POINTS 0 or less, defender disappears
+
+    //note to choose another opponent
+
+    //opponent moves to defender area
+    
+    //increase hero's ATTACK POWER only
+
+    //loop through game until either the hero's HEALTH POINTS are 0 or less OR all opponents are defeated
+
+});
